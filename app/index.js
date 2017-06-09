fb6d4df7875dfc7fa62e02cce36eedbe42f112f5
Tyler M <tylernm@gmail.com>
2017-06-08 17:55:38 -0700

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,42 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
+require('./index.css');
+var App = require('./components/App');
+
+// component handles:
+// state
+// lifecycle
+// ui
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
+
+//class Badge extends React.Component {
+  //render() {
+    //return (
+      //<div>
+        //<img src={this.props.img} />
+        //<h1>Name: {this.props.name} </h1>
+        //<h3>username: {this.props.username} </h3>
+      //</div>
+    //)
+  //}
+//}
+
+//Badge.propTypes = {
+  //img: PropTypes.string.isRequired,
+  //name: PropTypes.string.isRequired,
+  //username: PropTypes.string.isRequired
+//}
+
+//ReactDOM.render(
+  //<Badge
+    //name='Tyler McGinnis'
+    //username='tylermcginnis'
+    //img='https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'/>,
+  //document.getElementById('app')
+//);
